7b91e199db6b68ca8c593ca2df8e1b1afb15f0e3
beppemarinelli <marinellibeppe@gmail.com>
2023-08-30 22:11:21 +0200

    Merged better the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <Arduino.h>
 #include <RCSwitch.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <math.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
-// Create a new instance of the RCSwitch class for receiving messages from the remote
+/* Create a new instance of the RCSwitch class for receiving messages from the remote */
 RCSwitch ReceiverModule = RCSwitch();
+/* Assign a unique ID to this sensor */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-// Constants
+/* Constants */
 const int ACTUATOR_RELAY_PIN = 4;
 const int SERIAL_BAUD_RATE = 9600;
 
-// Receiver values
+/* Receiver values */
 const unsigned long RECEIVER_UP_VALUE = 2678888;
 const unsigned long RECEIVER_MIDDLE_VALUE = 2678884;
 const unsigned long RECEIVER_DOWN_VALUE = 2678882;
 const int RECEIVER_PROTOCOL = 1;
 const int RECEIVER_BIT_LENGTH = 24;
 
-// Enum for the RF signals
+/* Enum for the RF signals */
 enum RFSignal {
     RF_UP = 1,
     RF_MIDDLE = 2,
@@ -23,32 +30,57 @@ enum RFSignal {
     RF_NONE = 0
 };
 
-// Function declarations
+
+dataRate_t dataRate = ADXL345_DATARATE_100_HZ;
+
+/* Get angle for the y axis of the accelerometer */
+double get_angle_around_y_axis(double x, double y, double z);
+/* RF function method */
 RFSignal receiveRFSignals();
 
-// Setup for the arudino
+/* Setup for the arudino */
 void setup()
 {
-    // Setup the serial connection with 9600 baud rate
+    /* Setup the serial connection with 9600 baud rate */
     Serial.begin(SERIAL_BAUD_RATE);
 
-    // This needs to be digital pin 2 on the arduino
+    /* This needs to be digital pin 2 on the arduino */
     ReceiverModule.enableReceive(0); // Receiver on interrupt 0 => that is pin #2
 
-    // Make the actuator relay pin as output
+    /* Make the actuator relay pin as output */
     pinMode(ACTUATOR_RELAY_PIN, OUTPUT);
+
+    /* Initialise the sensor */
+    if (!accel.begin())
+    {
+        /* There was a problem detecting the ADXL345 ... check your connections */
+        Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+        while (1)
+            ;
+    }
+
+    /* Set the range to whatever is appropriate for your project */
+    accel.setRange(ADXL345_RANGE_2_G);
 }
 
-// Loop for the arduino
+/* Loop for the arduino */
 void loop()
 {
-    // Receive the rf signals
+    /* Get a new sensor event */
+    sensors_event_t event;
+    accel.getEvent(&event);
+
+    /* Print the angle */
+    Serial.print("Angle : ");
+    Serial.println(get_angle_around_y_axis(event.acceleration.x, event.acceleration.y, event.acceleration.z) * 180 / M_PI);
+
+    /* Receive the rf signals */
     int received_signal = receiveRFSignals();
 
-    // Print the received signal
+    /* Print the received signal */
     // Serial.println(received_signal);
 
-    // if the signal is up, then turn off the relay
+    /* If the signal is up, then turn off the relay */
     if (received_signal == RF_UP)
     {
         digitalWrite(ACTUATOR_RELAY_PIN, LOW);
@@ -57,22 +89,25 @@ void loop()
         digitalWrite(ACTUATOR_RELAY_PIN, HIGH);
     }
 
-    // Wait for 100ms
+    /* Wait for 100ms */
     delay(100);
 }
 
 /**
- * @brief Receive the rf signals from the remote
- * @return int
+ * @brief Receive the RF signals from the remote.
+ * 
+ * This function checks if the receiver module is available and if a signal has been received. If a signal has been received, it checks the value, bit length, and protocol of the signal and returns the corresponding RFSignal enum value. If no signal has been received, it returns RF_NONE.
+ * 
+ * @return The RFSignal enum value corresponding to the received signal, or RF_NONE if no signal has been received.
  */
 RFSignal receiveRFSignals()
 {
-    // Return value
+    /* Return value */
     RFSignal returnValue = RF_NONE;
 
     if (ReceiverModule.available())
     {
-        // Detect the received value
+        /* Detect the received value */
         if (ReceiverModule.getReceivedValue() == RECEIVER_UP_VALUE && ReceiverModule.getReceivedBitlength() == RECEIVER_BIT_LENGTH && ReceiverModule.getReceivedProtocol() == RECEIVER_PROTOCOL)
         {
             returnValue = RF_UP;
@@ -86,98 +121,25 @@ RFSignal receiveRFSignals()
             returnValue = RF_DOWN;
         }
 
-        // Reset the receiver
+        /* Reset the receiver */
         ReceiverModule.resetAvailable();
     }
 
     return returnValue;
 }
 
-// IMU reader
-
-// #include <Arduino.h>
-// #include <Wire.h>
-// #include <Adafruit_Sensor.h>
-// #include <Adafruit_ADXL345_U.h>
-// /* Assign a unique ID to this sensor at the same time */
-// /* Uncomment following line for default Wire bus      */
-// Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
-
-// dataRate_t dataRate = ADXL345_DATARATE_100_HZ;
-
-// /* NeoTrellis M4, etc.                    */
-// /* Uncomment following line for Wire1 bus */
-// // Adafruit_ADXL343 accel = Adafruit_ADXL343(12345, &Wire1);
-
-// void displaySensorDetails(void)
-// {
-//     sensor_t sensor;
-//     accel.getSensor(&sensor);
-//     Serial.println("------------------------------------");
-//     Serial.print("Sensor:       ");
-//     Serial.println(sensor.name);
-//     Serial.print("Driver Ver:   ");
-//     Serial.println(sensor.version);
-//     Serial.print("Unique ID:    ");
-//     Serial.println(sensor.sensor_id);
-//     Serial.print("Max Value:    ");
-//     Serial.print(sensor.max_value);
-//     Serial.println(" m/s^2");
-//     Serial.print("Min Value:    ");
-//     Serial.print(sensor.min_value);
-//     Serial.println(" m/s^2");
-//     Serial.print("Resolution:   ");
-//     Serial.print(sensor.resolution);
-//     Serial.println(" m/s^2");
-//     Serial.println("------------------------------------");
-//     Serial.println("");
-//     delay(500);
-// }
-
-// void setup(void)
-// {
-//     Serial.begin(9600);
-//     while (!Serial)
-//         ;
-//     Serial.println("Accelerometer Test");
-//     Serial.println("");
-
-//     /* Initialise the sensor */
-//     if (!accel.begin())
-//     {
-//         /* There was a problem detecting the ADXL345 ... check your connections */
-//         Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-//         while (1)
-//             ;
-//     }
-
-//     /* Set the range to whatever is appropriate for your project */
-//     // accel.setRange(ADXL345_RANGE_16_G);
-//     // accel.setRange(ADXL343_RANGE_8_G);
-//     accel.setRange(ADXL345_RANGE_4_G);
-//     // accel.setRange(ADXL343_RANGE_2_G);
-
-//     /* Display some basic information on this sensor */
-//     displaySensorDetails();
-//     Serial.println("");
-// }
-
-// void loop(void)
-// {
-//     /* Get a new sensor event */
-//     sensors_event_t event;
-//     accel.getEvent(&event);
-
-//     /* Display the results (acceleration is measured in m/s^2) */
-//     Serial.print("X: ");
-//     Serial.print(event.acceleration.x);
-//     Serial.print("  ");
-//     Serial.print("Y: ");
-//     Serial.print(event.acceleration.y);
-//     Serial.print("  ");
-//     Serial.print("Z: ");
-//     Serial.print(event.acceleration.z);
-//     Serial.print("  ");
-//     Serial.println("m/s^2 ");
-//     delay(500);
-// }
+/**
+ * Calculates the angle around the y-axis in radians.
+ *
+ * @param x The x-coordinate of the point.
+ * @param y The y-coordinate of the point (not used in the calculation).
+ * @param z The z-coordinate of the point.
+ *
+ * @return The angle around the y-axis in radians.
+ */
+double get_angle_around_y_axis(double x, double y, double z)
+{
+    double projection = x * cos(M_PI / 2) + z * sin(M_PI / 2);
+    double angle = atan2(projection, x);
+    return angle;
+}
